96dd50da0e3951b41dcf8c36c4c2c6920d235879
Bishal <pauljeet2003@gmail.com>
2024-09-03 09:53:28 +0530

    Updated

---
+++ Square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    cout<<"The square of " <<num <<" is: " <<(num * num);
+
+    return 0;
+}
